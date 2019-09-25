474873cd62c7454a8f423fdfa8be87c56f00cb2f
Nestor Demeure <nestor@nestor-HP-ProBook-650-G1>
2019-09-25 21:01:26 +0200

    cleaned multivariate normal code

--- src/multivariate_normal.rs
+++ src/multivariate_normal.rs
@@ -2,27 +2,23 @@ use nalgebra::{DMatrix, DVector};
 use rand::Rng;
 use rand_distr::StandardNormal;
 
-/// modelize a multidimensional multivariate normal distribution
-/// multidimensional because each sample is a matrix and not just a vector
+/// modelize a multivariate normal distribution
 pub struct MultivariateNormal
 {
    mean: DVector<f64>,
-   cholesky_cov_inputs: DMatrix<f64>,
+   cholesky_covariance: DMatrix<f64>
 }
 
 impl MultivariateNormal
 {
    /// outputs a new multivariate guassian with the given parameters
-   pub fn new(mean: DVector<f64>,
-              covariance_inputs: DMatrix<f64>)
-              -> Self
+   pub fn new(mean: DVector<f64>, covariance: DMatrix<f64>) -> Self
    {
-      // covariance between the input points
-      let cholesky_cov_inputs = covariance_inputs.cholesky().expect("Cholesky decomposition failed!").unpack();
-      MultivariateNormal { mean, cholesky_cov_inputs }
+      let cholesky_covariance = covariance.cholesky().expect("Cholesky decomposition failed!").unpack();
+      MultivariateNormal { mean, cholesky_covariance }
    }
 
-   /// outputs the mans of the distribution
+   /// outputs the mean of the distribution
    pub fn mean(&self) -> &DVector<f64>
    {
       &self.mean
@@ -31,9 +27,7 @@ impl MultivariateNormal
    /// takes a random number generator and uses it to sample from the distribution
    pub fn sample<RNG: Rng>(&self, rng: &mut RNG) -> DVector<f64>
    {
-      // TODO can we just generate a vector from a fucntion ?
-      let normals = DMatrix::from_fn(self.mean.nrows(), 1, |_, _| rng.sample(StandardNormal));
-      // TODO is there a blas operation to make this operation faster ?
-      &self.mean + &self.cholesky_cov_inputs * normals
+      let normal = DVector::from_fn(self.mean.nrows(), |_, _| rng.sample(StandardNormal));
+      &self.mean + &self.cholesky_covariance * normal
    }
 }
